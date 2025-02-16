579317b66f54f47ed51a2beb2b8d0727170910ca
tash-2s <81064017+tash-2s@users.noreply.github.com>
2025-02-15 20:22:21 -0800

    remove Altair

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -16,7 +16,6 @@
 import "./App.scss";
 import { LiveAPIProvider } from "./contexts/LiveAPIContext";
 import SidePanel from "./components/side-panel/SidePanel";
-import { Altair } from "./components/altair/Altair";
 import ControlTray from "./components/control-tray/ControlTray";
 
 const API_KEY = process.env.REACT_APP_GEMINI_API_KEY as string;
@@ -36,7 +35,6 @@ function App() {
           <main>
             <div className="main-app-area">
               {/* APP goes here */}
-              <Altair />
               <div id="yt-embed">
                 <YoutubeEmbed yurl="https://www.youtube.com/watch?v=sOFmYwYa9Pk" />
               </div>
